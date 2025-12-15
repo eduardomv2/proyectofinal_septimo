cf29d631ce348e85072bde56126dfbae461079bb
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2025-12-14 18:00:59 -0600

    Add project files.

---
+++ proyectofinal_septimo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyectofinal_septimo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
